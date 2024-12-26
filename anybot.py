31cc9e731563619cde50caf7fa528a8cf4c89181
Q u e e n <aridwis203@gmail.com>
2024-12-26 14:22:25 +0700

    Update structure files and folder

--- anybot.py
+++ anybot.py
@@ -1,13 +1,12 @@
-import mangofaucet
+import __bot__.faucet.mangofaucet as mangofaucet
 
-def main_menu():
-    print("============= Anybot Menu =============")
-    print("1. Bot faucet Mangowallet")
-    choice = input("Pilih menu: ")
-    
-    if choice == "1":
+def faucet_menu():
+    print("1. Mango faucet")
+    faucet_choice = input("Choice bot faucet:")
+
+    if faucet_choice == "1":
         env_choice = input("ENV (1 untuk Devnet, 2 untuk Testnet): ")
-        
+           
         if env_choice == "1":
             env = "devnet"
         elif env_choice == "2":
@@ -15,11 +14,19 @@ def main_menu():
         else:
             print("Pilihan ENV tidak valid!")
             return
-        
+            
         address = input("Address: ")
         mangofaucet.mangoFaucet(env, address)
     else:
         print("Pilihan menu tidak valid!")
+
+def main_menu():
+    print("============= Anybot Menu =============")
+    print("1. Bot faucet")
+    choice = input("Choice bot type: ")
+    
+    if choice == "1":
+        faucet_menu()
 
 if __name__ == "__main__":
     main_menu()
